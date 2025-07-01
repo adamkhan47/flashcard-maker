72088f442c9428bb69db66bceccb463145132a4c
Adam Khan <adammkhan47@gmail.com>
2025-06-30 22:27:24 -0500

    reading is done. ALL debug stuff tho

--- script.js
+++ script.js
@@ -2,8 +2,30 @@ function importQuizlet() {
 
 }
 function exportFunc() {
-    alert(document.getElementById('inputText').value);
+
 }
 function save() {
-
+    const textareacontent = document.getElementById('inputText').value;
+    const lines = textareacontent.split('\n').length;
+    let term = [];
+    let def = [];
+    let arrayOfContent = textareacontent.split('\n');
+    let successful = false;
+    for (let i = 0; i<lines; i++) {
+        const arr = arrayOfContent[i].split("|")
+        if (arr.length < 2 === false) {
+            term.push(arr[0]);
+            def.push(arr[1]);
+            successful = true;
+        }
+        else {
+            alert("Invalid data.");
+            successful = false;
+            break;
+        }
+    }
+    if (successful) {
+        alert(term);
+        alert(def);
+    }
 }
